dc39ecde776da994bc3a121649a44cf1fa116a8a
Hasmukh Bhadani <54616621+hasmukhbhadani86@users.noreply.github.com>
2021-08-21 16:34:02 +0530

    Update ExtUtils.kt

--- ExtUtils.kt
+++ ExtUtils.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.view.View
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
-
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.widget.ProgressBar
+import androidx.annotation.IntegerRes
 
 fun Context.toast(message: String){
     Toast.makeText(this, message, Toast.LENGTH_LONG ).show()
@@ -21,15 +24,63 @@ fun View.invisible(){
     visibility = View.INVISIBLE
 }
 
-fun View.snackBar(message: String)
-{
-    Snackbar.make(this, message, Snackbar.LENGTH_LONG).show()
+@SuppressLint("ResourceType")
+inline fun View.snack(@IntegerRes messageRes: Int, length: Int = Snackbar.LENGTH_LONG, f: Snackbar.() -> Unit) {
+    snack(resources.getString(messageRes), length, f)
 }
 
-fun View.snackBarAction(message: String){
-    Snackbar.make(this, message, Snackbar.LENGTH_LONG).also { snackbar ->
-        snackbar.setAction("Ok") {
-            snackbar.dismiss()
-        }
-    }.show()
+
+inline fun View.snack(message: String, length: Int = Snackbar.LENGTH_LONG, f: Snackbar.() -> Unit) {
+    val snack = Snackbar.make(this, message, length)
+    snack.f()
+    snack.show()
 }
+
+fun View.showSnack(message: String, length: Int = Snackbar.LENGTH_LONG) {
+    val snack = Snackbar.make(this, message, length)
+    snack.show()
+}
+
+inline fun View.errorSnack(message: String, length: Int = Snackbar.LENGTH_LONG, f: Snackbar.() -> Unit) {
+    val snack = Snackbar.make(this, message, length)
+    snack.f()
+    snack.setActionTextColor(Color.parseColor("#FFFFFF"))
+    snack.view.setBackgroundColor(Color.parseColor("#C62828"))
+    snack.show()
+}
+
+fun View.errorSnack(message: String, length: Int = Snackbar.LENGTH_LONG) {
+    val snack = Snackbar.make(this, message, length)
+    snack.setActionTextColor(Color.parseColor("#FFFFFF"))
+    snack.view.setBackgroundColor(Color.parseColor("#C62828"))
+    snack.show()
+}
+
+@SuppressLint("ResourceType")
+fun Snackbar.action(@IntegerRes actionRes: Int, color: Int? = null, listener: (View) -> Unit) {
+    action(view.resources.getString(actionRes), color, listener)
+}
+
+fun Snackbar.action(action: String, color: Int? = null, listener: (View) -> Unit) {
+    setAction(action, listener)
+    color?.let { setActionTextColor(color) }
+}
+
+fun Context.toast(message: String){
+    Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+}
+
+fun ProgressBar.show(){
+    this.visibility = View.VISIBLE
+}
+
+fun ProgressBar.hide(){
+    this.visibility = View.GONE
+}
+
+
+//----------------------------------- How to use-----------------------------------
+//Store value into the Share preference:
+  view.errorSnack(message,Snackbar.LENGTH_LONG)
+  context.toast("Hello World")
+
